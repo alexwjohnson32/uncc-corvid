482b3024bd3318751ec24aba124148f678b11da4
David Stowe <david.stowe@corvidtec.com>
2025-06-30 11:24:06 -0400

    Recovered gridpack-gridlab build option and improved multi-stage build script.

--- multi_stage_build/build.sh
+++ multi_stage_build/build.sh
@@ -16,6 +16,7 @@ if [ "$#" -ne 1 ]; then
    echo "                     + builds."
    echo "'helics'           - rebuilds helics and all dependencies"
    echo "'gridlab-gridpack' - builds Gridlab-D then GridPACK only"
+   echo "'gridpack-gridlab' - builds GridPACK then Gridlab-D only"
    echo "'gridlab'          - builds Gridlab-D only"
    echo "'gridpack'         - builds GridPACK only"
    exit 1
@@ -24,31 +25,21 @@ fi
 if [ $1 == "sys" ]; then
    declare -i start=0
    declare -a stages=("sys" "helics" "gridlab" "gridpack")
-   rm rl8_uncc_${stages[0]}.sif
-   rm rl8_uncc_${stages[0]}_${stages[1]}.sif
-   rm rl8_uncc_${stages[0]}_${stages[1]}_${stages[2]}.sif
-   rm rl8_uncc_${stages[0]}_${stages[1]}_${stages[2]}_${stages[3]}.sif
 elif [ $1 == "helics" ]; then
    declare -i start=1
    declare -a stages=("sys" "helics" "gridlab" "gridpack")
-   rm rl8_uncc_${stages[0]}_${stages[1]}.sif
-   rm rl8_uncc_${stages[0]}_${stages[1]}_${stages[2]}.sif
-   rm rl8_uncc_${stages[0]}_${stages[1]}_${stages[2]}_${stages[3]}.sif
 elif [ $1 == "gridlab-gridpack" ]; then
-   ls *.sif
    declare -i start=2
    declare -a stages=("sys" "helics" "gridlab" "gridpack")
-   rm rl8_uncc_${stages[0]}_${stages[1]}_${stages[2]}.sif
-   rm rl8_uncc_${stages[0]}_${stages[1]}_${stages[2]}_${stages[3]}.sif
-   ls *.sif
+elif [ $1 == "gridpack-gridlab" ]; then
+   declare -i start=2
+   declare -a stages=("sys" "helics" "gridpack" "gridlab")
 elif [ $1 == "gridlab" ]; then
    declare -i start=3
    declare -a stages=("sys" "helics" "gridpack" "gridlab")
-   rm rl8_uncc_${stages[0]}_${stages[1]}_${stages[2]}_${stages[3]}.sif
 elif [ $1 == "gridpack" ]; then
    declare -i start=3
    declare -a stages=("sys" "helics" "gridlab" "gridpack")
-   rm rl8_uncc_${stages[0]}_${stages[1]}_${stages[2]}_${stages[3]}.sif
 else
    echo "Invalid option"
    echo ""
@@ -56,30 +47,42 @@ else
    echo "'sys'              - rebuilds system and all dependencies (everything)"
    echo "'helics'           - rebuilds helics and all dependencies (except sys)"
    echo "'gridlab-gridpack' - rebuilds Gridlab-D then GridPACK"
+   echo "'gridpack-gridlab' - rebuilds GridPACK then Gridlab-D"
    echo "'gridlab'          - rebuilds Gridlab-D only"
    echo "'gridpack'         - rebuilds GridPACK only"
    exit 1
 fi
 
 # Check if necessary *.sif files exist
-if [ $start -eq 1 ]; then
+if [ $start -eq 0 ]; then
+   rm rl8_uncc_${stages[0]}.sif
+   rm rl8_uncc_${stages[0]}_${stages[1]}.sif
+   rm rl8_uncc_${stages[0]}_${stages[1]}_${stages[2]}.sif
+   rm rl8_uncc_${stages[0]}_${stages[1]}_${stages[2]}_${stages[3]}.sif
+elif [ $start -eq 1 ]; then
    if [ ! -f rl8_uncc_${stages[0]}.sif ]; then
       echo rl8_uncc_${stages[0]}.sif does not exist.
       echo "You need to run with the " ${stages[0]} " build option."
       exit 1
    fi
+   rm rl8_uncc_${stages[0]}_${stages[1]}.sif
+   rm rl8_uncc_${stages[0]}_${stages[1]}_${stages[2]}.sif
+   rm rl8_uncc_${stages[0]}_${stages[1]}_${stages[2]}_${stages[3]}.sif
 elif [ $start -eq 2 ]; then
    if [ ! -f rl8_uncc_${stages[0]}_${stages[1]}.sif ]; then
       echo rl8_uncc_${stages[0]}_${stages[1]}.sif does not exist.
       echo "You need to run with the " ${stages[1]} " build option."
       exit 1
    fi
+   rm rl8_uncc_${stages[0]}_${stages[1]}_${stages[2]}.sif
+   rm rl8_uncc_${stages[0]}_${stages[1]}_${stages[2]}_${stages[3]}.sif
 elif [ $start -eq 3 ]; then
    if [ ! -f rl8_uncc_${stages[0]}_${stages[1]}_${stages[2]}.sif ]; then
       echo rl8_uncc_${stages[0]}_${stages[1]}_${stages[2]}.sif does not exist.
       echo "You need to run with the " ${stages[2]}-${stages[3]} "build option."
       exit 1
    fi
+   rm rl8_uncc_${stages[0]}_${stages[1]}_${stages[2]}_${stages[3]}.sif
 fi
 
 for i in $(seq $start 3);
