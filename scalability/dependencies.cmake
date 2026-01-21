b351b7365fa20200298c909bae640cb70b9fdc95
Luke Williamson <luke.williamson@corvidtec.com>
2026-01-21 17:40:25 -0500

    Removed old directories and files

--- scalability/dependencies.cmake
+++
@@ -1,5 +0,0 @@
-set(Boost_USE_STATIC_LIBS ON)
-find_package(Boost 1.78 REQUIRED COMPONENTS system filesystem json mpi random)
-
-message(STATUS "Found Boost INCLUDE: ${Boost_INCLUDE_DIRS}")
-message(STATUS "Found Boost LIBS: ${Boost_LIBRARIES}")
