db32ca5281f97846f493f33f87c27d5638c69082
Luke Williamson <luke.williamson@corvidtec.com>
2026-04-03 17:40:39 -0400

    got the deploy initially setup

--- deploy/deploy.py
+++ deploy/deploy.py
@@ -11,6 +11,8 @@ def main():
 
     _setup_list_names_parser(subparsers)
     _setup_get_exec_parser(subparsers)
+    _setup_list_source_files_parser(subparsers)
+    _setup_list_model_files_parser(subparsers)
 
     args = top_parser.parse_args()
 
@@ -23,6 +25,10 @@ def main():
             _print_plugin_names(plugin_manager)
         elif args.command == "get-exec":
             _print_federate_definition(plugin_manager, args.plugin_name, args.model_name, pathlib.Path(args.deploy_root))
+        elif args.command == "list-source-files":
+            _print_list_source_files(plugin_manager, args.plugin_name, pathlib.Path(args.install_dir))
+        elif args.command == "list-model-files":
+            _print_list_model_files(plugin_manager, args.model_name, pathlib.Path(args.deploy_dir), args.model_name)
         else:
             print(f"Unrecognized Command '{args.command}', terminating.")
 
@@ -48,6 +54,30 @@ def _setup_get_exec_parser(subparser: argparse._SubParsersAction) -> None:
 def _print_federate_definition(plugin_manager: manager.PluginManager, plugin_name: str, model_name: str, deploy_root: pathlib.Path) -> None:
     print(f"Federate Executable String for {model_name}:")
     print(commands.get_federate_definition.get_federate_definition(plugin_manager, model_name, deploy_root, plugin_name))
+
+def _setup_list_source_files_parser(subparser: argparse._SubParsersAction) -> None:
+    list_source_files_parser = subparser.add_parser("list-source-files", help="Attempts to list the given source files for a plugin "
+                                                    + "located within the install directory")
+
+    list_source_files_parser.add_argument("--plugin-name", required=True, help="The name of the plugin to get input files for", type=str)
+    list_source_files_parser.add_argument("--install-dir", required=True, help="The path to the install directory to read from", type=str)
+
+def _print_list_source_files(plugin_manager: manager.PluginManager, plugin_name: str, install_dir: pathlib.Path) -> None:
+    print(f"Source Files for {plugin_name}")
+    print(f"list-source-files command, plugin-name: {plugin_name}, install-dir: {str(install_dir)}")
+
+def _setup_list_model_files_parser(subparser: argparse._SubParsersAction) -> None:
+    list_model_files_parser = subparser.add_parser("list-model-files", help="Attempts to list the given model files for a plugin and model name "
+                                                   + "located within the deploy directory")
+
+    list_model_files_parser.add_argument("--model-name", required=True, help="The name of the model to get input files for", type=str)
+    list_model_files_parser.add_argument("--deploy-dir", required=True, help="The path to the deploy directory to read from", type=str)
+    list_model_files_parser.add_argument("--plugin-name", required=False, help="The name of the plugin to search from to find the model's input files", type=str)
+
+def _print_list_model_files(plugin_manager: manager.PluginManager, model_name: str, deploy_dir: pathlib.Path, plugin_name: str) -> None:
+    print(f"Model Files for {model_name}")
+    print(f"list-model-files command, plugin_name: {plugin_name}, deploy_dir: {str(deploy_dir)}, model_name: {model_name}")
+
 
 if __name__ == '__main__':
     main()
